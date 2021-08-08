aead5d6cd3f1ffd77cb1b795a329e42ebed70d41
itslhun <nahuelghastin@outlook.com>
2021-08-08 23:04:37 +0200

    game logic is no longer dependant on animation frames, instead tied to interval to avoid performance issues on higher/lower refresh rates

--- js/game.js
+++ js/game.js
@@ -16,74 +16,94 @@ class Game {
     this.running = true;
     this.globalScore = 0;
     const tutorialZero = new TutorialOne(this, 'WELCOME', 'A JOURNEY BEGINS');
-    this.levels.push(tutorialZero)
-    // const levelTwo = new LevelTwo(this, 'NOT ALONE', 'MEETING A FAMILIAR FACE');
-    // this.levels.push(levelTwo)
-   // const levelThree = new LevelThree(this, 'HOP HOP', 'A BLOCK WITH NO LEGS');
-   // this.levels.push(levelThree)
-   // const levelFour = new LevelFour(this, 'TWITCHY FINGERS', 'EVERYBODY\'S GONE FLAPPY');
-   // this.levels.push(levelFour)
-   // const scoreLevel = new LevelScore(this, 'CONGRATULATIONS', 'YOU COMPLETED THE CHAlLENGES');
-   // this.levels.push(scoreLevel)
+    this.levels.push(tutorialZero);
+    const levelTwo = new LevelTwo(this, 'NOT ALONE', 'MEETING A FAMILIAR FACE');
+    this.levels.push(levelTwo);
+    const levelThree = new LevelThree(this, 'HOP HOP', 'A BLOCK WITH NO LEGS');
+    this.levels.push(levelThree);
+    const levelFour = new LevelFour(
+      this,
+      'TWITCHY FINGERS',
+      "EVERYBODY'S GONE FLAPPY"
+    );
+    this.levels.push(levelFour);
+    const scoreLevel = new LevelScore(this, 'CONGRATULATIONS', 'YOU COMPLETED THE CHAlLENGES');
+    this.levels.push(scoreLevel)
 
     this.activeLevel = tutorialZero;
     this.scoreCounter = new ScoreCounter(this, 15, 15);
+
+    this.startLogicInterval();
     this.loop();
   }
   increaseGlobalScore() {
     this.globalScore++;
   }
 
+  startLogicInterval(){
+    this.logicID = setInterval(() => {
+      if (!this.activeLevel.player.deadTimeout) {
+      this.runLogic();
+      }
+    }, 15);
+  }
+
   loop() {
     switch (this.currentLevelIndex) {
       case 0:
-        if (!this.levels[0].musicRunning){
+        if (!this.levels[0].musicRunning) {
           this.levels[0].start();
-          const levelOne = new LevelTwo(this, 'NOT ALONE', 'MEETING A FAMILIAR FACE');
+          const levelOne = new LevelTwo(
+            this,
+            'NOT ALONE',
+            'MEETING A FAMILIAR FACE'
+          );
           this.levels.push(levelOne);
         }
         this.activeLevel = this.levels[0];
         break;
       case 1:
-        if (!this.levels[1].musicRunning){
+        if (!this.levels[1].musicRunning) {
           this.levels[1].start();
-          const levelTwo = new LevelThree(this, 'HOP HOP', 'A BLOCK WITH NO LEGS');
-          this.levels.push(levelTwo)
+          // const levelTwo = new LevelThree(this, 'HOP HOP', 'A BLOCK WITH NO LEGS');
+          // this.levels.push(levelTwo)
         }
         this.activeLevel = this.levels[1];
         break;
       case 2:
-        if (!this.levels[2].musicRunning){
+        if (!this.levels[2].musicRunning) {
           this.levels[2].start();
-          const levelThree = new LevelFour(this, 'TWITCHY FINGERS', 'EVERYBODY\'S GONE FLAPPY');
-          this.levels.push(levelThree)
+          // const levelThree = new LevelFour(this, 'TWITCHY FINGERS', 'EVERYBODY\'S GONE FLAPPY');
+          // this.levels.push(levelThree)
         }
         this.activeLevel = this.levels[2];
         break;
       case 3:
-        if (!this.levels[3].musicRunning){
+        if (!this.levels[3].musicRunning) {
           this.levels[3].start();
-          const scoreLevel = new LevelScore(this, 'CONGRATULATIONS', 'YOU COMPLETED THE CHAlLENGES');
-          this.levels.push(scoreLevel)
+          // const scoreLevel = new LevelScore(this, 'CONGRATULATIONS', 'YOU COMPLETED THE CHAlLENGES');
+          // this.levels.push(scoreLevel)
         }
         this.activeLevel = this.levels[3];
         break;
-        case 4:
-          if (!this.levels[4].musicRunning){
-            this.levels[4].start();
-          }
+      case 4:
+        if (!this.levels[4].musicRunning) {
+          this.levels[4].start();
+        }
         this.activeLevel = this.levels[4];
         break;
     }
     this.paint();
 
-    if (!this.activeLevel.player.deadTimeout) {
-      this.runLogic();
-    }
+    // if (!this.activeLevel.player.deadTimeout) {
+    //   this.runLogic();
+    // }
     if (this.running) {
       window.requestAnimationFrame(() => {
         this.loop();
       });
+    } else {
+      clearInterval(this.logicID);
     }
   }
 
